4c3346a0ffb8af6bcef91c642d4a2d1a2831cf39
Macchiato <simppp@gmail.com>
2024-09-02 12:57:29 +0900

    애니메이션 클립 미러 프로그램 초안 작성

---
+++ Assets/Editor/AnimationMirrorer.cs
@@ -0,0 +1,44 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+
+/*
+ * VRSuya AnimationMirrorer
+ * Contact : vrsuya@gmail.com // Twitter : https://twitter.com/VRSuya
+ */
+
+namespace com.vrsuya.animationcleaner {
+
+	public class AnimationMirrorer : Editor {
+
+		[MenuItem("Assets/VRSuya/Mirror Animation")]
+		public static void MirrorSelectedAnimation() {
+			foreach (var SelectedObject in Selection.objects) {
+				if (!SelectedObject || !(SelectedObject is AnimationClip)) {
+					return;
+				}
+				AnimationClip OriginalAnimationClip = SelectedObject as AnimationClip;
+				AnimationClip MirroredAnimationClip = new AnimationClip();
+				MirroredAnimationClip.name = OriginalAnimationClip.name + "_Mirrored";
+				EditorUtility.CopySerialized(OriginalAnimationClip, MirroredAnimationClip);
+				EditorCurveBinding[] Bindings = AnimationUtility.GetCurveBindings(MirroredAnimationClip);
+				foreach (EditorCurveBinding Binding in Bindings) {
+					AnimationCurve NewCurve = AnimationUtility.GetEditorCurve(MirroredAnimationClip, Binding);
+					for (int Index = 0; Index < NewCurve.keys.Length; Index++) {
+						Keyframe OriginalKey = NewCurve.keys[Index];
+						OriginalKey.value = -OriginalKey.value;
+						NewCurve.MoveKey(Index, OriginalKey);
+					}
+					AnimationUtility.SetEditorCurve(MirroredAnimationClip, Binding, NewCurve);
+				}
+				string OriginalAssetPath = AssetDatabase.GetAssetPath(OriginalAnimationClip);
+				string NewAssetPath = System.IO.Path.GetDirectoryName(OriginalAssetPath) + "/" + MirroredAnimationClip.name + "_Mirrored.anim";
+				AssetDatabase.CreateAsset(MirroredAnimationClip, NewAssetPath);
+				AssetDatabase.SaveAssets();
+				Debug.Log("[VRSuya] " + NewAssetPath + " 애니메이션 클립을 생성하였습니다.");
+			}
+			return;
+		}
+	}
+}
+#endif
