ac7ea7a853b58a7ef1be37798e504eb91512d1f7
Macchiato <simppp@gmail.com>
2024-09-06 08:21:58 +0900

    YAML을 분석하여 라인을 정리하는 프로그램 작성

---
+++ Assets/Editor/AnimatorControllerCleanerEditor.cs
@@ -0,0 +1,73 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+/*
+ * VRSuya AnimatorControllerCleaner Editor
+ * Contact : vrsuya@gmail.com // Twitter : https://twitter.com/VRSuya
+ */
+
+namespace com.vrsuya.animationcleaner {
+
+	[CustomEditor(typeof(AnimatorControllerCleaner))]
+	public class AnimatorControllerCleanerEditor : EditorWindow {
+
+		SerializedObject SerializedObject;
+		AnimatorControllerCleaner AnimatorControllerCleanerInstance;
+
+		SerializedProperty SerializedTargetAnimatorController;
+		SerializedProperty SerializedTargetfileIDs;
+
+		void OnEnable() {
+			if (!AnimatorControllerCleanerInstance) {
+				AnimatorControllerCleanerInstance = CreateInstance<AnimatorControllerCleaner>();
+			}
+			SerializedObject = new SerializedObject(AnimatorControllerCleanerInstance);
+			SerializedTargetAnimatorController = SerializedObject.FindProperty("TargetAnimatorController");
+			SerializedTargetfileIDs = SerializedObject.FindProperty("TargetfileIDs");
+		}
+
+		[MenuItem("Tools/VRSuya/AnimatorController Cleaner", priority = 1000)]
+		static void CreateWindow() {
+			AnimatorControllerCleanerEditor AppWindow = (AnimatorControllerCleanerEditor)GetWindow(typeof(AnimatorControllerCleanerEditor));
+			AppWindow.titleContent = new GUIContent("AnimatorController Cleaner");
+			return;
+		}
+
+		void OnGUI() {
+			SerializedObject.Update();
+			EditorGUILayout.Space(EditorGUIUtility.singleLineHeight);
+			GUILayout.BeginHorizontal();
+			GUILayout.FlexibleSpace();
+			GUILayout.Label("정리할 AnimatorController", EditorStyles.boldLabel);
+			GUILayout.FlexibleSpace();
+			GUILayout.EndHorizontal();
+			GUILayout.BeginHorizontal();
+			GUILayout.FlexibleSpace();
+			EditorGUILayout.PropertyField(SerializedTargetAnimatorController, GUIContent.none);
+			GUILayout.FlexibleSpace();
+			GUILayout.EndHorizontal();
+			EditorGUILayout.Space(EditorGUIUtility.singleLineHeight);
+			GUILayout.BeginHorizontal();
+			GUILayout.FlexibleSpace();
+			GUILayout.Label("제거할 fileID 리스트", EditorStyles.boldLabel);
+			GUILayout.FlexibleSpace();
+			GUILayout.EndHorizontal();
+			GUILayout.BeginHorizontal();
+			GUILayout.FlexibleSpace();
+			EditorGUILayout.PropertyField(SerializedTargetfileIDs, GUIContent.none);
+			GUILayout.FlexibleSpace();
+			GUILayout.EndHorizontal();
+			GUILayout.BeginHorizontal();
+			GUILayout.FlexibleSpace();
+			EditorGUILayout.Space(EditorGUIUtility.singleLineHeight);
+			if (GUILayout.Button("정리", GUILayout.Width(100))) {
+				AnimatorControllerCleaner.RemoveStructureByFileID();
+			}
+			GUILayout.FlexibleSpace();
+			GUILayout.EndHorizontal();
+			EditorGUILayout.Space(EditorGUIUtility.singleLineHeight);
+			SerializedObject.ApplyModifiedProperties();
+			return;
+		}
+	}
+}
---
+++ Assets/Runtime/AnimatorControllerCleaner.cs
@@ -0,0 +1,107 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+using UnityEditor;
+using UnityEditor.Animations;
+using UnityEngine;
+
+/*
+ * VRSuya AnimatorControllerCleaner
+ * Contact : vrsuya@gmail.com // Twitter : https://twitter.com/VRSuya
+ */
+
+namespace com.vrsuya.animationcleaner {
+
+	[ExecuteInEditMode]
+	public class AnimatorControllerCleaner : ScriptableObject {
+
+		[SerializeField]
+		public static AnimatorController TargetAnimatorController = null;
+		public static string[] TargetfileIDs = new string[0];
+
+		private static string AssetFilePath = string.Empty;
+		private static readonly string fileIdPattern = @"fileID:\s*(\d+)";
+
+		/// <summary>파일에서 해당 되는 fileID과 연계된 라인들을 모두 지웁니다.</summary>
+		public static void RemoveStructureByFileID() {
+			if (TargetAnimatorController && TargetfileIDs.Length > 0) {
+				AssetFilePath = AssetDatabase.GetAssetPath(TargetAnimatorController);
+				if (!string.IsNullOrEmpty(AssetFilePath)) {
+					string[] AssetFile = File.ReadAllLines(AssetFilePath);
+					List<int> RemoveLineIndex = new List<int>();
+					foreach (string TargetfileID in TargetfileIDs) {
+						AnimatorControllerCleaner newInstance = new AnimatorControllerCleaner();
+						RemoveLineIndex.AddRange(newInstance.GetRemoveLines(AssetFile, TargetfileID));
+					}
+					if (RemoveLineIndex.Count > 0) {
+						List<string> newAssetFile = new List<string>(AssetFile);
+						int[] RemoveLineIndexs = RemoveLineIndex.ToArray();
+						Array.Sort(RemoveLineIndexs);
+						Array.Reverse(RemoveLineIndexs);
+						foreach (int TargetIndex in RemoveLineIndexs) {
+							if (TargetIndex >= 0 && TargetIndex < newAssetFile.Count) {
+								newAssetFile.RemoveAt(TargetIndex);
+							}
+						}
+						File.WriteAllLines(AssetFilePath, newAssetFile.ToArray());
+						Debug.LogWarning("[AnimatorControllerCleaner] AnimatorController에서 총 " + RemoveLineIndex.Count + "줄의 데이터가 정리 되었습니다!");
+					} else {
+						Debug.Log("[AnimatorControllerCleaner] AnimatorController의 모든 구성요소가 유효합니다!");
+					}
+				}
+			}
+			return;
+		}
+
+		/// <summary>파일에서 fileID에 해당되는 라인 인덱스들을 반환 합니다.</summary>
+		/// <returns>삭제해야 될 Int 형태의 Index 리스트</returns>
+		private List<int> GetRemoveLines(string[] TargetFile, string TargetFileID) {
+			List<int> RemoveLineIndex = new List<int>();
+			bool isDeleting = false;
+			string StructureStartPattern = $"--- !u!";
+			for (int Line = 0; Line < TargetFile.Length; Line++) {
+				if (TargetFile[Line].StartsWith(StructureStartPattern) && TargetFile[Line].Contains($"&{TargetFileID}")) {
+					isDeleting = true;
+					RemoveLineIndex.Add(Line);
+					continue;
+				}
+				if (isDeleting && TargetFile[Line].StartsWith(StructureStartPattern) && !TargetFile[Line].Contains($"&{TargetFileID}")) {
+					isDeleting = false;
+					break;
+				}
+				if (isDeleting && !string.IsNullOrEmpty(ExtractFileIDFromLine(TargetFile[Line]))) {
+					RemoveLineIndex.Add(Line);
+					RemoveLineIndex.AddRange(GetRemoveLines(TargetFile, ExtractFileIDFromLine(TargetFile[Line])));
+				}
+				if (isDeleting) {
+					RemoveLineIndex.Add(Line);
+				}
+			}
+			return RemoveLineIndex;
+		}
+
+		/// <summary>해당 라인에 유효한 fileID가 있다면 해당 값을 반환합니다.</summary>
+		/// <returns>String 형태의 fileID</returns>
+		private string ExtractFileIDFromLine(string Line) {
+			Match fileIDMatch = Regex.Match(Line, fileIdPattern);
+			if (fileIDMatch.Success) {
+				return fileIDMatch.Groups[1].Value;
+			} else {
+				return string.Empty;
+			}
+		}
+
+		/// <summary>요청한 GUID를 파일 이름으로 반환합니다. 2번째 인자는 확장명 포함 여부를 결정합니다.</summary>
+		/// <returns>파일 이름</returns>
+		private string GUIDToAssetName(string GUID, bool OnlyFileName) {
+			string FileName = "";
+			FileName = AssetDatabase.GUIDToAssetPath(GUID).Split('/')[AssetDatabase.GUIDToAssetPath(GUID).Split('/').Length - 1];
+			if (OnlyFileName) FileName = FileName.Split('.')[0];
+			return FileName;
+		}
+	}
+}
+#endif
