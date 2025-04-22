3358d06085b8c2082d2f4bc2dcb93871b7ab2094
Macchiato <simppp@gmail.com>
2025-04-23 05:20:38 +0900

    CAU 정렬 프로그램 초안 추가

---
+++ Assets/Editor/SortCAU.cs
@@ -0,0 +1,24 @@
+﻿#if CONTINUOUS_AVATAR_UPLOADER
+using UnityEditor;
+using UnityEngine;
+
+using Anatawa12.ContinuousAvatarUploader;
+
+/*
+ * VRSuya Cleaner
+ * Contact : vrsuya@gmail.com // Twitter : https://twitter.com/VRSuya
+ * Forked from https://forum.unity.com/threads/get-animator-in-editor-mode.461838/
+ */
+
+namespace com.vrsuya.cleaner {
+
+	[ExecuteInEditMode]
+	public class SortCAU : EditorWindow {
+
+		[MenuItem("Tools/VRSuya/Cleaner/Sort CAU", priority = 1000)]
+		public static void SortCAUAssets() {
+			return;
+		}
+	}
+}
+#endif
